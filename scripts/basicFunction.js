3a7cb3e3483d1b631f001b6c766aae1142db29a7
eduardomy <eduardo.medina@cetys.edu.mx>
2016-12-15 13:06:58 -0800

    A better navbar and more html files

---
+++ scripts/basicFunction.js
@@ -0,0 +1,34 @@
+window.onresize=setHeader;
+let horizontalMenuIsHidden=false;
+setHeader();
+
+function setHeader(){
+    if(window.innerWidth<756 && !horizontalMenuIsHidden){
+	console.log("hi");
+	document.getElementsByClassName("pure-menu-list")[0].style.display="none";
+	organizeItems("block");
+	document.getElementsByClassName("fa-bars")[0].style.display="block";
+	horizontalMenuIsHidden=true;
+    }
+    else if(horizontalMenuIsHidden){
+	document.getElementsByClassName("pure-menu-list")[0].style.display="inline-block";
+	organizeItems("inline-block");
+	document.getElementsByClassName("fa-bars")[0].style.display="none";
+	horizontalMenuIsHidden=false;
+    }
+    console.log(window.innerWidth);
+}
+function organizeItems(typeDisplay){
+    let menuItems;
+    document.getElementsByClassName("pure-menu-heading")[0].style.display=typeDisplay;
+    menuItems=document.getElementsByClassName("pure-menu-item");
+    for(c=0; c<menuItems.length; c++)
+	menuItems[c].style.display=typeDisplay;
+}
+
+function displayOptions(){
+    if(document.getElementsByClassName("pure-menu-list")[0].style.display=="none")
+	document.getElementsByClassName("pure-menu-list")[0].style.display="inline-block";
+    else
+	document.getElementsByClassName("pure-menu-list")[0].style.display="none";
+}
