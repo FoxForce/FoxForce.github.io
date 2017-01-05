c5380260bf9a88b318061310bf720313aad44d65
eduardomy <eduardo.medina@cetys.edu.mx>
2017-01-05 13:03:29 -0800

    More work in progress

--- scripts/basicFunction.js
+++ scripts/basicFunction.js
@@ -5,7 +5,6 @@ setHeader();
 
 function setHeader(){
     if(window.innerWidth<1024 && !horizontalMenuIsHidden){
-	console.log("hi");
 	document.getElementsByClassName("pure-menu-list")[0].style.display="none";
 	organizeItems("block");
 	document.getElementsByClassName("fa-bars")[0].style.display="block";
@@ -17,11 +16,11 @@ function setHeader(){
 	document.getElementsByClassName("fa-bars")[0].style.display="none";
 	horizontalMenuIsHidden=false;
     }
-    console.log(window.innerWidth);
 }
 
 function organizeItems(typeDisplay){
     let menuItems;
+
     document.getElementsByClassName("pure-menu-heading")[0].style.display=typeDisplay;
     menuItems=document.getElementsByClassName("pure-menu-item");
     for(c=0; c<menuItems.length; c++)
@@ -29,13 +28,15 @@ function organizeItems(typeDisplay){
 }
 
 function displayOptions(){
-    if(document.getElementsByClassName("pure-menu-list")[0].style.display=="none")
+    if(document.getElementsByClassName("pure-menu-list")[0].style.display=="none"){
 	document.getElementsByClassName("pure-menu-list")[0].style.display="inline-block";
-    else
+    }
+    else{
 	document.getElementsByClassName("pure-menu-list")[0].style.display="none";
+    }
 }
 
 function setHeaderAndFooter(){
-    document.getElementById('header').innerHTML='<div class="pure-menu pure-menu-horizontal pure-menu-fixed"><a class="pure-menu-heading" href="index.html"> Fox Force</a><a onClick="displayOptions()"><i class="fa-menu fa fa-bars fa-2x" style="display:none" aria-hidden="true"></i></a><ul class="pure-menu-list"><li class="pure-menu-item"> <a class="pure-menu-link" href="about.html"> Acerca de Nosotros </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="history.html"> Ediciones Previas </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="sponsors.html"> Patrocinadores </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="donate.html"> Donar </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="contact.html"> Contactanos </a> </li></ul></div>';
+    document.getElementById('header').innerHTML='<div class="pure-menu pure-menu-horizontal pure-menu-fixed"><a class="pure-menu-heading" href="index.html">Fox Force</a><a onClick="displayOptions()"><i class="fa-menu fa fa-bars fa-2x" style="display:none" aria-hidden="true"></i></a><ul class="pure-menu-list"><li class="pure-menu-item"> <a class="pure-menu-link" href="about.html"> Acerca de Nosotros </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="history.html"> Ediciones Previas </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="sponsors.html"> Patrocinadores </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="donate.html"> Donar </a> </li><li class="pure-menu-item"> <a class="pure-menu-link" href="contact.html"> Contactanos </a> </li></ul></div>';
     document.getElementById('footer').innerHTML='<p class="footer-text"> &middot; Encuentranos &middot; </p><center><a href="http://www.facebook.com/foxforcesae"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a><a href="https://github.com/FoxForce"> <i class="fa fa-github fa-2x" aria-hidden="true"></i></a><a href="https://www.instagram.com/fox.force.aerodesign/"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></center>';
 }
