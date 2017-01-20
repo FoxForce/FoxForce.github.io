c8fc1e54bcb93e18a968a8503d377c9b4ba22b9c
eduardomy <eduardo.medina@cetys.edu.mx>
2017-01-20 00:14:20 -0800

    more work, a better design, it will become even better with time

--- scripts/basicFunction.js
+++ scripts/basicFunction.js
@@ -7,5 +7,7 @@ document.getElementById('menu').onclick=function(){
 setHeaderAndFooter();
 
 function setHeaderAndFooter(){
-    document.getElementById('footer').innerHTML='<center><a href="http://www.facebook.com/foxforcesae"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a><a href="https://github.com/FoxForce"> <i class="fa fa-github fa-2x" aria-hidden="true"></i></a><a href="https://www.instagram.com/fox.force.aerodesign/"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></center>';
+    
+    document.getElementById('footer').innerHTML='<a href="mailto:foxforce@cetys.edu.mx"> <i class="fa fa-envelope fa-4x"></i></a> <a href="http://www.facebook.com/foxforcesae"><i class="fa fa-facebook-official fa-4x" aria-hidden="true"></i></a><a href="https://www.instagram.com/fox.force.aerodesign/"><i class="fa fa-instagram fa-4x" aria-hidden="true"></i></a>';
+    
 }
