8c219d0973a85e87e52a6f2bd04664c48c514150
Eduardo Medina <eduardo.medina@cetys.edu.mx>
2017-02-12 18:10:34 -0800

    An improvement

--- scripts/basicFunction.js
+++ scripts/basicFunction.js
@@ -7,12 +7,28 @@ document.getElementById('menu').onclick=function(){
     return false;
 };
 
+$(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = document.getElementById('header-image').height;             // set to whatever you want it to be
+
+    if(y_scroll_pos > scroll_pos_test) {
+        $('.navbar').css('background-color', 'black');
+        //$('.navbar').fadeOut();
+    }
+    else
+        $('.navbar').css('background-color', 'transparent');
+});
+
 function setHeaderAndFooter(){
+    /*
     if(location.pathname.split('/').slice(-1) === 'index.html')
 	      document.getElementById('header').innerHTML='<nav class="navbar navbar-default"><div class="navbar-header"><center><a class="navbar-brand"  href="#"><img alt="Brand" src="resources/Fox_IconWhite.png" style="margin-left:2em;" width="70em" height="40em"></a><a href="#" id="menu"> <i class="fa fa-bars fa-3x menu"  aria-hidden="true"></i> </a><ul id="menu-options" class="nav navbar-nav"><li> <a href="#acerca"> <big>S</big>obre <big>n</big>osotros </a> </li><li> <a href="#contacto"> <big>C</big>ontáctanos </a> </li><li> <a href="#donar"> <big>D</big>onar </a> </li></ul></center></div></nav>';
     else
 	document.getElementById('header').innerHTML='<nav class="navbar navbar-default"><div class="container-fluid"><div class="navbar-header"><a class="navbar-brand"  href="index.html"><img alt="Brand" src="resources/Fox_IconWhite.png" style="margin-left:2em;" width="70em" height="40em"></a><a href="index.html" id="menu"> <i class="fa fa-bars fa-3x menu"  aria-hidden="true"></i> </a><ul id="menu-options" class="nav navbar-nav"><li> <a class="text-uppercase" href="index.html#acerca"> <big>S</big>obre <big>n</big>osotros </a> </li><li> <a class="text-uppercase" href="index.html#contacto"> <big>C</big>ontáctanos </a> </li><li> <a class="text-uppercase" href="#donar"> <big>D</big>onar </a> </li></ul></div></div></nav>';
-    
+    */
     document.getElementById('footer').innerHTML='<div class="container"> <div class="row"><div class="col-md-6 text-align-left"> <div class="col-md-6 col-xs-6"><p> Proyecto de Aeroespacial de SAE Aerodesign</p><p>Ubicación: Mexicali, B.C. <p>Correo: foxforce@cetys.edu.mx</p> </div> <div class="col-md-6 col-xs-6"> <p class="big">Enlaces:</p><p><a href="index.html"> Inicio </a> </p> <p> Acerca </p> <p> Ediciones pasadas </p> <p> </p></div> </div> <div class="col-md-6 text-align-right"><a href="mailto:foxforce@cetys.edu.mx"> <i class="fa fa-envelope fa-3x"></i></a> <a href="http://www.facebook.com/foxforcesae"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a><a href="https://www.instagram.com/fox.force.aerodesign/"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a></div> </div></div>';
     
+}
+function moveToText(){
+    window.scroll({top:  document.getElementById('header-image').height, left:0, behavior:'smooth'});
 }
